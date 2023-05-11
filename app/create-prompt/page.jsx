8d1237f4d5d0f2e816a89b5cc20b1df0f5beb498
El-Behery <dev.elbehery@gmail.com>
2023-05-12 02:46:08 +0300

    create post page is done, mongodb, mongoose, models

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -6,13 +6,39 @@ import { useRouter } from "next/navigation";
 import { Form } from "@components";
 
 const CreatePrompt = () => {
+  const router = useRouter();
+  const { data: session } = useSession();
+
   const [submitting, setSubmitting] = useState(false);
   const [post, setPost] = useState({
     prompt: "",
     tag: "",
   });
 
-  const createPrompt = async () => {};
+  const createPrompt = async (e) => {
+    e.preventDefault();
+    setSubmitting(() => true);
+
+    try {
+      const response = await fetch("/api/prompt/new", {
+        method: "POST",
+        body: JSON.stringify({
+          prompt: post.prompt,
+          userId: session?.user.id,
+          tag: post.tag,
+        }),
+      });
+
+      if (response.ok) {
+        // homepage
+        router.push("/");
+      }
+    } catch (err) {
+      console.error(err);
+    } finally {
+      setSubmitting(() => false);
+    }
+  };
 
   return (
     <Form
--- components/Form.jsx
+++ components/Form.jsx
@@ -11,7 +11,7 @@ const Form = ({ type, post, setPost, submitting, handleSubmit }) => {
         imagination run wild with any AI-powered platform.
       </p>
       <form
-        onSubmit={(e) => e.preventDefault()}
+        onSubmit={handleSubmit}
         className="mt-10 w-full max-w-2xl flex flex-col gap-7 glassmorphism"
       >
         <label>
